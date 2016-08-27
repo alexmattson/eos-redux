571344a60ecb09afaefe22a76c31717633c30a60
amattson21 <amattson21@gmail.com>
2016-08-27 12:41:09 -0700

    added a webpack.config.js file

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+module.exports = {
+  context: __dirname,
+  entry: './frontend/index.jsx',
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+        test: /\.node$/,
+        loader: 'node-loader'
+      }
+    ]
+  },
+  devtool: 'source-maps'
+};
