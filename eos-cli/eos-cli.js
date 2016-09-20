e4f46458b924ca60069fcf16dd2b458ee7b2c1bb
paulmoliva <paulmoliva@gmail.com>
2016-09-20 12:50:08 -0700

    resolved server generation bug

--- eos-cli/eos-cli.js
+++ eos-cli/eos-cli.js
@@ -7,13 +7,14 @@ const Actions = require('./actions/actions.js');
 // PROGRAM //
 
 const program = require('commander');
-let name;
+let name, start;
 program
   .version('0.0.1')
   .arguments('<cmd> [env1] [env2]')
   .action(function (cmd, env1, env2) {
     // Start
     if (['start', 's'].includes(cmd)) {
+      start = true;
       name = env1;
       Actions.start(env1);
     }
@@ -34,4 +35,4 @@ program
 
 
 program.parse(process.argv);
-if (program.backend) Actions.backend(name, program.backend);
+if (start) Actions.backend(name, program.backend);
