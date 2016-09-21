7faa1971b75d7cff497cac7b6fb88815d5c3424a
amattson21 <amattson21@gmail.com>
2016-09-21 07:47:21 -0700

    cars removed from middleware and reducer

--- templates/start/master_middleware.js
+++ templates/start/master_middleware.js
@@ -1,8 +1,7 @@
 import { applyMiddleware } from 'redux';
-import CarsMiddleware from './cars_middleware';
 
 const masterMiddleware = applyMiddleware(
-	CarsMiddleware
+	
 );
 
 export default masterMiddleware;
--- templates/start/root_reducer.js
+++ templates/start/root_reducer.js
@@ -1,8 +1,7 @@
 import { combineReducers } from 'redux';
-import CarsReducer from './cars_reducer';
 
 const RootReducer = combineReducers({
-	cars: CarsReducer
+
 });
 
 export default RootReducer;
